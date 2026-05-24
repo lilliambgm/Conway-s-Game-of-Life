cd2e728bdd4f033e8b997543a194351757b82af8
lilliambgm <lilliambgm@gmail.com>
2026-05-24 05:07:57 +0200

    Updated to DetermineStarterState
    Changed constructor to actually set the current cell object state, not the global cell state.
    Added DetermineNewState method, which should take in all neighbour states at call.
    This will then update the state based upon the rules of the Game of Life.
    Added comments for all methods, and trying to explain what I want to achieve with everything.
    Added ToDo items for upcoming changes.
    Added UpdateState method for updating the state outside of this class.
    Simplified the code for determining the starter state.

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,18 +1,59 @@
 public class Cell {
-    String state;
+    private String state;
 
+    //Constructor, setting the state to a random state determined by DetermineStarterState()
     public String cell() {
-        determineState();
-        return state;
+        DetermineStarterState();
+        return this.state;
     }
 
-    private void determineState() {
+    //Function using Java.Math, to determine a random cell state, either "-" or "0"
+    private void DetermineStarterState() {
+        // Generating a random number between 0 and 1.
         double random = Math.random();
         if (random < 0.5) {
+            // Setting state to dead (-) if random number is smaller than .5
             this.state = "-";
-        }
-        if (random > 0.5) {
+        } else {
+            // Setting the state to live (0) if random number is .5 or greater.
             this.state = "0";
         }
+    }
+
+    // Determining the new cell state, by considering the neighbour of the current cell, which will be given in the function call.
+    // ToDo Make sure that the updated cells are not taken into consideration for cells later on in the game grid.
+    private void DetermineNewState(String[] neighbours) {
+        // Declaring variable to keep count of the neighbours that are alive.
+        int aliveNeighbours = 0;
+        // Initiating an enhanced for loop to loop over the neighbour array passed into the function.
+        for (String neighbourState : neighbours) {
+            //Checking neighbour state, and adding to the alive count if state is 0 (which is the alive state)
+            if (neighbourState.equals("0")) {
+                aliveNeighbours += 1;
+            }
+        }
+        // Applying the rules of the Game of Life for live cells, as per stated in Main.java
+        // ToDo Simplify this into only needing one check for keeping the cell alive, killing the cell with a simple "else" statement.
+        if (this.state.equals("0")) {
+            if (aliveNeighbours < 2) {
+                // Killing the cell with less than 2 live cells
+                this.state = "-";
+            } else if (aliveNeighbours == 2 || aliveNeighbours == 3) {
+                // Keeping the cell alive with 2 or 3 live cells
+                this.state = "0";
+            } else {
+                // Killing the cell in all other situations.
+                this.state = "-";
+            }
+        }
+        // Checking if the cell is dead, and has exactly 3 live cells, reviving this cell.
+        if (this.state.equals("-") && aliveNeighbours == 3) {
+            this.state = "0";
+        }
+    }
+
+    // Updating cell state based on passed through state.
+    public void UpdateState(String state) {
+        this.state = state;
     }
 }
