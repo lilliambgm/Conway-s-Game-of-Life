fa72831e0f46842bb41c0b27537afe75ca2c5996
lilliambgm <lilliambgm@gmail.com>
2026-05-27 09:31:00 +0200

    Simple main class added, which will pull all components for the game from other classes.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,3 +5,17 @@
  * 3. Any live cell with more than three live neighbours dies, as if by overpopulation;
  * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by repopulation.
  */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    // Defining variables we're using throughout the whole game.
+    protected static int width;
+    protected static int height;
+    // Setting up the scanner as I think
+    protected static Scanner input = new Scanner(System.in);
+
+    static void main(String[] args) {
+    }
+}
