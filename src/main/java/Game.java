f8bc1156f3fcd7205dc1c382460c6e1b81fbb0ef
LilliamBGM <lilliambgm@gmail.com>
2026-05-28 15:10:20 +0200

    Updated the constructor, so that it will prompt the user for the height and width, and use these as the game dimensions.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,8 +6,10 @@
 public class Game {
     String[][] game;
     Cell[][] gameCells;
-    public Game(int height, int width) {
-        game =  new String[height][width];
+    public Game() {
+        int height = Integer.parseInt(Helper.prompter("What would you like the height of the game to be?"));
+        int width = Integer.parseInt(Helper.prompter("What would you like the width of the game to be?"));
+        game = new String[height][width];
         gameCells = new Cell[height][width];
     }
 }
