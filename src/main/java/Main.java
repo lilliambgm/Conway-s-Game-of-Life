ad7e418d656fe5398dc2fd22706167ea8964fbd4
LilliamBGM <lilliambgm@gmail.com>
2026-05-30 17:15:18 +0200

    Added functionality for going to the next frame, however this is subject to change, as this is less neat looking than what I'm going for.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * The rules for Conway's game of Life are as follows:
  * 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation;
@@ -11,5 +13,14 @@
 public class Main {
     static void main(String[] args) {
         Game game = new Game();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type \"Next\" or \"N\" to advance the game 1 frame, or type \"Exit\" to exit");
+        int counter = 0;
+        String userInput = input.next();
+        while (!userInput.equals("Exit") && userInput.equals("Next") || userInput.equals("N")) {
+            counter++;
+            System.out.printf("Frame %3$d of your randomly generated game of life with height %1$d and width %2$d:\n", game.height, game.width, counter);
+            userInput = input.next();
+        }
     }
 }
